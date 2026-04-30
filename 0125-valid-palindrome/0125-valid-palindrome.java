a413400ca00246b0274830db12c5b8ca151b42d8
Rajesh Rauta <rajeshrauta9427@gmail.com>
2026-04-30 14:49:05 +0530

    Time: 3 ms (60.13%), Space: 44.9 MB (44.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (('a' <= c && c <= 'z') || ('0' <= c && c <= '9')) {
+                sb.append(c);
+            }
+        }
+        int left = 0;
+        int right = sb.length() - 1;
+
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
